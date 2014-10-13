8a79c67ff735c7bd33ed537d77abb74e27eaf02e
Ben Burkert <ben@benburkert.com>
2014-10-13 15:12:50 +0200

    Update the package docs

--- doc.go
+++ doc.go
@@ -1,4 +1,15 @@
-// Package tune provides a mechanism for creating TCP listeners with socket options
-// not available with net.ListenTCP.
+/*
+Package tune provides a mechanism for creating TCP listeners with socket options
+not available with net.ListenTCP.
+
+Example Usage
+
+The `tune.TuneAndListen` function works the same as `net.Listen` with
+[self-referential functions for options](http://commandcenter.blogspot.nl/2014/01/self-referential-functions-and-design.html).
+
+	// sets SO_REUSEPORT on the socket
+	listener, err := tune.TuneAndListen("tcp", "0.0.0.0:80", tune.ReusePort)
+
+*/
 
 package tune
